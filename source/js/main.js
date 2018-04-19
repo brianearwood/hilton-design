e3e310b33a8d7a9ebde415ffaecc7fc17129e376
Brian Earwood <brianearwood@gmail.com>
2018-04-19 17:05:23 -0500

    Making lots of css/html updates to be responsive

--- source/js/main.js
+++ source/js/main.js
@@ -29,6 +29,14 @@
         $(window).resize(function() {
           self.resizeModals();
         });
+
+        if( $('body').height() < $(window).height() ) {
+          $('.page-nav').addClass('extended');
+          $('body').css({'overflow': 'hidden'});
+        } else {
+          $('.page-nav').removeClass('extended');
+          $('body').css({'overflow': 'visible'});
+        }
     },
 
     megamenu: function () {
@@ -49,21 +57,58 @@
 
       // mobile expandable menu
       var mql = window.matchMedia('(max-width: 576px)');
+      // var short_mql = window.matchMedia('(max-height: 400px)');
+
+      var screenResults = {
+        'skinny': false,
+        'short':  false
+      }
+      
       function screenTest(e) {
         if (e.matches) {
-          /* the viewport is 576 pixels wide or less */
+          /* the viewport is 576 pixels wide or 400 px tall or less */
+          console.log("skinny");
           $(".megamenu__chapter").addClass('collapsed');
           $(".megamenu__heading").on("click", function (event) {
-            $(this).parent().toggleClass("collapsed");
+            event.preventDefault();
+            $('.megamenu__chapter').each(function() { $(this).addClass('collapsed'); })
+            $(this).parent().removeClass("collapsed");
           });
+          screenResults['skinny'] = true;
         } else {
-          /* the viewport is more than than 576 pixels wide */
+          /* the viewport is more than than 576 pixels wide or 400 tall */
+          console.log("fat");
           $(".megamenu__chapter").removeClass('collapsed');
           $(".megamenu__heading").off("click");
+          screenResults['skinny'] = false;
         }
       }
+      // function shortTest(e) {
+      //   if ( e.matches && screenResults['skinny'] ) {
+      //     /* the viewport is 400 px tall or less */
+      //     console.log("short");
+      //     $(".megamenu__chapter").addClass('collapsed');
+      //     $(".megamenu__heading").on("click", function (event) {
+      //       event.preventDefault();
+      //       $('.megamenu__chapter').each(function() { $(this).addClass('collapsed'); })
+      //       $(this).parent().removeClass("collapsed");
+      //     });
+
+      //   } else {
+      //     /* the viewport is more than than 576 pixels wide or 400 tall */
+      //     console.log("tall");
+      //     $(".megamenu__chapter").removeClass('collapsed');
+      //     $(".megamenu__heading").off("click");
+      //   }
+      // }
+
       mql.addListener(screenTest);
+      // short_mql.addListener(shortTest);
+
       screenTest(mql);
+      // shortTest(short_mql);
+
+
     },
 
     search: function () {
@@ -87,24 +132,24 @@
       $('.fullpage').fullpage({
         sectionSelector: '.fp-section',
         fixedElements: '.global__header, .global__footer',
-        // paddingTop: '103px',
-        // paddingBottom: '48px',
+        paddingTop: '72px',
+        // paddingBottom: '32px',
         lazyLoading: false,
         menu: '#section-menu',
-        scrollOverflow: true,
+        // scrollOverflow: true,
         responsiveWidth: 992,
         responsiveHeight: 800,
         bigSectionsDestination: 'top',
-        fitToSection: false,
+        fitToSection: true,
         afterResponsive:  function(isResponsive){
           // $.fn.fullpage.destroy('all');
           if (isResponsive) {
             $.fn.fullpage.setAutoScrolling(false);
-            $("#section-menu").addClass("no-display");
+            $('.global__header').appendTo( $('.global:first-child') );
           }
           else {
             $.fn.fullpage.setAutoScrolling(true);
-            $("#section-menu").removeClass("no-display");
+            $('.global__header').insertBefore( $('.global__footer') );
           }
           
         }
@@ -155,7 +200,13 @@
       // capture event from Bootstrap Modal js 
       $(document).on('shown.bs.modal', function (e) {
         HH.resizeModals();
+        $.fn.fullpage.setAutoScrolling(false);
+        $('body').addClass('overflow-hidden-imp');
       });
+      $(document).on('hidden.bs.modal', function (e) {
+        $.fn.fullpage.setAutoScrolling(true);
+        $('body').removeClass('overflow-hidden-imp');
+      })
 
       // $(document).on('click', '[data-toggle="modal"]', function (event) {
         
